0f2c136a0c9790327b777bfc3fdf0c66f3322828
adam <aryczkowski@imgw.pl>
2018-03-03 13:14:47 +0100

    Update R

--- update-R.sh
+++ update-R.sh
@@ -3,32 +3,65 @@
 localrmirror="/media/adam-minipc/other"
 remotemirror="http://cran.us.r-project.org"
 
-if [ ! -d ${localrmirror}/r-mirror ]; then
-	if mount ${localrmirror}; then
-		if [ -d ${localrmirror}/r-mirror ]; then
-			remotemirror="file:///${localrmirror}/r-mirror"
+function get_home_dir {
+	echo $( getent passwd "$USER" | cut -d: -f6 )
+}
+
+function get_other_folder {
+	folder="$1"
+	if [ -n "${folder}" ]; then
+		folder="/${folder}"
+	fi
+	mountpoint="/media/adam-minipc/other"
+	if [ -d "${mountpoint}" ]; then
+		if [ ! -d "${mountpoint}${folder}" ]; then
+			if ping -c 1 -w 1 ${adam-minipc}>/dev/null; then
+				mount ${mountpoint}
+			fi 
+		fi
+		if [ -d "${mountpoint}${folder}" ]; then
+			echo ${mountpoint}${folder}
 		fi
 	fi
-else
-	remotemirror="file:///${localrmirror}/r-mirror"
-fi
+	echo ""
+}
 
-if which R; then
+function get_debs_folder {
+	deb_folder=$(get_home_dir)/tmp/debs
+	other_folder=$(get_other_folder debs)
+	if [ -n "${other_folder}" ]; then
+		echo ${other_folder}
+	else
+		if [ ! -d ${deb_folder} ]; then
+			mkdir -p ${deb_folder}
+		fi
+		echo ${deb_folder}
+	fi
+}
+
+function update_r {
+	cran_folder=$(get_other_folder r-mirror)
+	if [ -n "${cran_folder}" ]; then
+		remotemirror="file:///${cran_folder}"
+	else
+		remotemirror="http://cran.us.r-project.org"
+	fi
+	sudo chown -R ${USER} $(get_home_dir)/R
+	sudo Rscript -e "update.packages(ask = FALSE, repos=\"${remotemirror}\")"
 	Rscript -e "update.packages(ask = FALSE, repos=\"${remotemirror}\")"
 	Rscript -e "if(!require(\"devtools\")) {install.packages(\"devtools\", ask=FALSE, repos=\"${remotemirror}\");devtools::install_github(\"hadley/devtools\")}"
 	Rscript -e 'if(!require("dtupdate")) devtools::install_github("hrbrmstr/dtupdate"); dtupdate::github_update()'
 	Rscript -e "update.packages(ask = FALSE, repos=\"${remotemirror}\")"
-	
-#	Rscript -e 'pkgs = loadedNamespaces();desc <- lapply(pkgs, packageDescription, lib.loc = NULL); for (d in desc) {if (!is.null(d$GithubSHA1)) {install_github(repo = d$GithubRepo, username = d$GithubUsername)}}'
-fi
+}
 
-if dpkg -s rstudio >/dev/null 2>/dev/null; then
+function update_rstudio {
 	ver=$(apt show rstudio | grep Version)
 	pattern='^Version: ([0-9.]+)\s*$'
 	if [[ $ver =~ $pattern ]]; then
 		ourversion=${BASH_REMATCH[1]}
 		netversion=$(Rscript -e 'cat(stringr::str_match(scan("https://www.rstudio.org/links/check_for_update?version=1.0.0", what = character(0), quiet=TRUE), "^[^=]+=([^\\&]+)\\&.*")[[2]])')
 		if [ "$ourversion" != "$netversion" ]; then
+			deb_folder=$(get_deb_folder)
 			RSTUDIO_URI=$(Rscript /tmp/get_rstudio_uri.R)
 			tee /tmp/get_rstudio_uri.R <<EOF
 if(!require('rvest')) install.packages('rvest', Ncpus=8, repos='${remotemirror}')
@@ -39,9 +72,10 @@ cat(html_node(thepage, xpath) %>% html_attr("href"))
 EOF
 			RSTUDIO_URI=$(Rscript /tmp/get_rstudio_uri.R)
 			
-			wget -c --output-document /tmp/rstudio.deb $RSTUDIO_URI 
-			sudo dpkg -i /tmp/rstudio.deb
-			rm /tmp/rstudio.deb
+			wget -c --output-document /tmp/rstudio.deb $RSTUDIO_URI -O ${deb_folder}/rstudio_${netvesion}_amd64.deb
+			if ! sudo dpkg -i ${deb_folder}/rstudio_${netvesion}_amd64.deb; then
+				sudo apt install -f --yes
+			fi
 			rm /tmp/get_rstudio_uri.R
 	
 			if fc-list |grep -q FiraCode; then
@@ -51,5 +85,47 @@ EOF
 			fi
 		fi
 	fi
+}
+
+function update_rstudio_server {
+	ver=$(apt show rstudio-server | grep Version)
+	pattern='^Version: ([0-9.]+)\s*$'
+	if [[ $ver =~ $pattern ]]; then
+		ourversion=${BASH_REMATCH[1]}
+		do_check=1
+	fi
+	netversion=$(wget --no-check-certificate -qO- https://s3.amazonaws.com/rstudio-server/current.ver)
+	if [ "$ourversion" != "$netversion" ]; then
+		deb_folder=$(get_deb_folder)
+		tee /tmp/get_rstudio_uri.R <<'EOT'
+if(!require('rvest')) install.packages('rvest', Ncpus=8, repos='http://cran.us.r-project.org')
+if(!require('stringr')) install.packages('stringr', Ncpus=8, repos='http://cran.us.r-project.org')
+xpath='code:nth-child(3)'
+url = "https://www.rstudio.com/products/rstudio/download-server/"
+thepage<-xml2::read_html(url)
+link<-html_node(thepage, xpath) %>% html_text()
+cat(stringr::str_match(link, '^\\$( wget)? (.*)$')[[3]])
+EOT
+		RSTUDIO_URI=$(sudo Rscript /tmp/get_rstudio_uri.R)
+		RSTUDIO_URI=$(sudo Rscript /tmp/get_rstudio_uri.R)
+		
+		wget -c $RSTUDIO_URI --output-document ${deb_folder}/rstudio-server_${netvesion}_amd64.deb
+		if ! sudo dpkg -i ${deb_folder}/rstudio-server_${netvesion}_amd64.deb; then
+			sudo apt install -f --yes
+		fi
+		rm /tmp/get_rstudio_uri.R
+	fi
+}
+
+if dpkg -s r-base-core >/dev/null 2>/dev/null; then
+	update_r
+fi
+
+if dpkg -s rstudio >/dev/null 2>/dev/null; then
+	update_rstudio
+fi
+
+if dpkg -s rstudio-server >/dev/null 2>/dev/null; then
+	update_rstudio_server
 fi
 
