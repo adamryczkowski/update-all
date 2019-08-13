e946c3055585e552f6d4b9c17cc5671ae2cf73ee
Adam Ryczkowski <adam@statystyka.net>
2019-08-13 09:41:22 +0200

    update conda

---
+++ update-conda.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! $(which conda >/dev/null) ]; then
+   conda update conda --yes
+   conda env list | grep -E '^[^ ]+ *\*? */.*$' | grep -Eo '^[^ ]+' | readarray -t
+   for myenv in "${MAPFILE[@]}"; do
+      conda update -n myenv --all
+   done
+   
+fi
+
