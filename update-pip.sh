e7fda647eb6b79da25b1561e08b35634c428fd06
Adam Ryczkowski <adam@statystyka.net>
2020-01-23 23:36:16 +0100

    Getting rid of updating python2

--- update-pip.sh
+++ update-pip.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if which pip2; then
-    sudo -H pip2 install --upgrade pip
-    sudo -H pip2 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo -H pip2 install -U
-fi 
+#if which pip2; then
+#    sudo -H pip2 install --upgrade pip
+#    sudo -H pip2 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo -H pip2 install -U
+#fi 
 if which pip3; then
     sudo -H pip3 install --upgrade pip
     sudo -H pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo -H pip3 install -U
